cff466cec19822fd63f04fdb2e0796435239b485
Alexey <Alexeipetrov97@mail.ru>
2022-03-10 21:53:26 +0700

    change to BY.CSS_SELECTOR and working with database

--- pars_main.py
+++ pars_main.py
@@ -1,14 +1,14 @@
-
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 import os
 import sqlite3
 
 class ParsHH:
 
+
     options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument('log-level=3')
 
     browser = webdriver.Chrome(chrome_options=options)
 
@@ -22,49 +22,91 @@ class ParsHH:
     )''') #создал таблицу
     db.commit()
 
-    list_vacancy = []
+#######################################
 
-    def __init__(self, link="https://novosibirsk.hh.ru/", imp_wait=0.1):
+    def __init__(self, link="https://novosibirsk.hh.ru/",
+                        search_profession='QA Automation',
+                        imp_wait=0.1):
         self.link = link
+        self.search_profession = search_profession
         self.imp_wait = imp_wait
-        self.browser.get(self.link)
+
+        self.list_vacancy = []
+
         self.browser.implicitly_wait(imp_wait)
 
+######################################
+
     def go_to_the_page(self):
-        element_seartch = self.browser.find_element_by_css_selector('input[data-qa="search-input"]')
+        self.browser.get(self.link)
+        element_seartch = self.browser.find_element(By.CSS_SELECTOR,'input[data-qa="search-input"]')
         element_seartch.click()
-        element_seartch.send_keys('QA Automation')
+        element_seartch.send_keys(self.search_profession)
         element_seartch.send_keys(Keys.ENTER)
 
     def search_vacancy(self):
-        count = self.browser.find_elements_by_css_selector('div.vacancy-serp-item')
+        count = self.browser.find_elements(By.CSS_SELECTOR,'div.vacancy-serp-item')
         for i in count:
-            self.list_vacancy = []
-            self.list_vacancy.append(i.find_element_by_css_selector('a[data-qa="vacancy-serp__vacancy-title"]').text)
-
+            list_vacancy_item = []
+            list_vacancy_item.append(i.find_element(By.CSS_SELECTOR,'a[data-qa="vacancy-serp__vacancy-title"]').text)
             try:
-                self.list_vacancy.append(i.find_element_by_css_selector('span[data-qa="vacancy-serp__vacancy-compensation"]').text)
+                list_vacancy_item.append(i.find_element(By.CSS_SELECTOR,'span[data-qa="vacancy-serp__vacancy-compensation"]').text)
             except:
-                self.list_vacancy.append('None')
+                list_vacancy_item.append('None')
 
-            self.list_vacancy.append(i.find_element_by_css_selector('a[data-qa="vacancy-serp__vacancy-employer"]').text)
-            self.list_vacancy.append(i.find_element_by_css_selector('a[data-qa="vacancy-serp__vacancy-title"]').get_attribute("href"))
-            self.sql.execute("INSERT INTO users VALUES(?, ?, ?, ?);", self.list_vacancy)
-            self.db.commit()
+            list_vacancy_item.append(i.find_element(By.CSS_SELECTOR,'a[data-qa="vacancy-serp__vacancy-employer"]').text)
+            list_vacancy_item.append(i.find_element(By.CSS_SELECTOR,'a[data-qa="vacancy-serp__vacancy-title"]').get_attribute("href"))
+            self.list_vacancy.append(list_vacancy_item)
+        return self.list_vacancy
+
+######################################
+
+
+    def save_db(self):
+        for val in self.list_vacancy:
+            self.sql.execute("INSERT INTO users VALUES(?, ?, ?, ?);", val)
+            self.db.commit()        
+
+    def close_db(self):
+        self.db.close()
+
+######################################
 
     def output_db(self):
+        out_db = []
         for val in self.sql.execute('SELECT * FROM users'):
-            print('Наименование вакансии: ' + val[0])
-            print('ЗП: ' + val[1])
-            print('Организация: ' + val[2])
-            print('Ссылка на ваканмию: ' + val[3])
-            print('------------------------------')
-        
+            out_db.append(val)
+        if len(out_db) == 0:
+            return 0
+        else:
+            return out_db
 
+
+    def output_new_vacancy(self):
+        vacancy_db = [val for val in self.sql.execute('SELECT * FROM users')]
+        new_vacancy = []
+        for val in self.list_vacancy:
+            if tuple(val) not in vacancy_db:
+                new_vacancy.append(val)
+        if len(new_vacancy) == 0:
+            return 0
+        else:
+            return new_vacancy
+            
+    def output_vacancy(self):
+        out_vacancy = self.search_vacancy()
+        if len(out_vacancy) == 0:
+            return 0
+        else:
+            return out_vacancy
 
 if __name__ == "__main__":
     os.system('cls')
     x = ParsHH()
     x.go_to_the_page()
     x.search_vacancy()
-    x.output_db()
+    x.output_new_vacancy()
+    #x.output_db()
+    x.save_db()
+    x.close_db()
+    
